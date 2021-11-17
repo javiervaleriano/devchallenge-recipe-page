28edfc98c997f534aea8bb15f75e84065a6b44a6
Javier Valeriano <jvalerianoz15@gmail.com>
2021-11-16 21:28:33 -0400

    Update alive.js

--- alive.js
+++ alive.js
@@ -2,7 +2,10 @@
 
 /* Add animation when images have finally loaded (one of the last DOM elements to load 100%) |
   Agrega la animación cuando las imágenes finalmente han cargado (de los últimos elementos del DOM en cargar al 100%) */
-window.addEventListener('load', () => document.querySelector('article').classList.add('article'));
+window.addEventListener('load', function() {
+    document.querySelector('#loading').style.display = 'none';
+    document.querySelector('article').classList.add('article');
+});
 
 /* Add a URL from the 'profiles' array randomly to the footer link |
  Agrega una dirección URL de la matriz 'profiles' aleatoriamente al enlace del footer */
